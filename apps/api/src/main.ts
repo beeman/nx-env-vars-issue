5db50591907175ab50c433007c2d08dca5aadde3
Bram Borggreve <beeman@beeman.nl>
2022-09-20 02:12:58 +0100

    chore: show issue

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,6 +9,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
+  if (!process.env.TEST_ENV_VAR) {
+    throw new Error(`TEST_ENV_VAR not set`);
+  }
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
